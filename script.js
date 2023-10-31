cf6e4e45c3e1bf4217ac0635937f044e809ec56f
Rayden22 <29240299+Rayden5@users.noreply.github.com>
2023-10-31 11:31:13 -0700

    Update comments and function names
    Tried to make comments more for the why than the what. Added a missing
    semicolon and updated function names for better readability.
    
    [Ticket: 3]

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-//Gets computer choice
+//Gets computer choice for the game
 function makeComputerChoice() {
-    //Sets randNum to 1, 2, or 3
+    //randNum is 1, 2, or 3 for each game choice
     let randNum = Math.floor(Math.random() * 3) + 1;
-    //sets computer choice based on randNum value
     let choice = randNum == 1 ? "Rock" : randNum == 2 ? "Paper" : "Scissors";
     //computer choice is printed to console if you're curious
     console.log("choice: " + choice);
     return choice;
 }
 
-//Gets User Choice with error handling if player is snarky
+//Gets User Choice for the game
 function getUserInput() {
     let userInput = "";
 
-    //prompt for user choice. if user gives invalid response
-    //itll keep repeating until valid response is given
+    //prompt for user choice. Loop for if player gives no response or
+    //invalid response
     do {
         userInput = prompt("Rock, Paper, or Scissors?").toUpperCase();
     }
     while (!userInput || !(userInput == "ROCK" || userInput == "PAPER"
     || userInput == "SCISSORS"));
 
-    //converts user response to first letter capitalized string
-    //for later function reactability
+    //converts user response for later function readibility
     let userFirst = userInput.slice(0,1).toUpperCase();
     let userRest = userInput.slice(1).toLowerCase();
     userInput = userFirst + userRest;
@@ -31,28 +29,26 @@ function getUserInput() {
     return userInput;
 }
 
-//Checks users choice and reacts accordingly
+//Checks user choice to see what computer needs to lose
 function checkUserChoice(userInput, compChoice) {
-    //checking for tie first makes rest of checks simplified
+    //tie check to reduce repitition in each choice check
     if (userInput == compChoice) {
         return "Tie!";
     } else if (userInput == "Rock") {
-        //sends the choice that would make user lose
-        //compChoice is sent for comparative
-        //userInput only sent for result string
-        return checkWinner("Paper", userInput, compChoice);
+        //first arguement for easy player loss checking
+        return decideWinner("Paper", userInput, compChoice);
     } else if (userInput == "Paper") {
-        return checkWinner("Scissors", userInput, compChoice);
+        return decideWinner("Scissors", userInput, compChoice);
     } else if (userInput == "Scissors") {
-        return checkWinner("Rock", userInput, compChoice)
+        return decideWinner("Rock", userInput, compChoice);
     } else {
         //only hits if something went wrong
         console.log("yuh oh");
     }
 }
 
-//Determines the winner of the game
-function checkWinner(userLose, userInput, compChoice) {
+//Decides winner for result string
+function decideWinner(userLose, userInput, compChoice) {
     //since tie is already checked, only have to check if compChoice
     //is or isnt the choice to make user lose
     if (compChoice == userLose) {
@@ -62,8 +58,7 @@ function checkWinner(userLose, userInput, compChoice) {
     }
 }
 
-//runs the game once, then checks if user wants to keep playing
-//game repeats until user doesn't want to play anymore
+//Runs game. duh
 do {
     alert(checkUserChoice(getUserInput(), makeComputerChoice()));
 } while (confirm("Play Again?"));
