80945c7f4046563cf56101f5945e9995204f36cb
Rayden22 <29240299+Rayden5@users.noreply.github.com>
2023-10-31 11:08:20 -0700

    Update and finish README and js and add comments
    Fixed errors with wrong function names being called and invalid do/while
    loop syntax. Added comments for easy understanding of code logic.
    
    [Ticket: 2]

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rockPaperScissors
+
+Simple prompt game with useless buttons for now. Gets computer choice with
+a random number generator so user has a 1/3 chance to get any option. User
+prompt allows for invalid responses, any cased response, and no response.
+Made multiple game functions for less repeated code. By checking for tie
+before any other choices, reduces code and checks overall. If not a tie,
+that leaves only win or lose, which is checked in checkWinner function. Win
+is an easy if/else check since checkUserChoice checks the users input and what
+is needed to beat them. Game is endlessly replayable with a simple do/while
+loop calling the game to repeat until user cancels Play Again.
--- script.js
+++ script.js
@@ -1,73 +1,60 @@
-/*
-Take user input from button
-Store user input from selected button
-check if user input is empty, equal to rock, paper, and scissors
-if not, keep prompting till viable answer is given
-
-Get Random Math number
-Floor Random Math number
-Times Random Math number by 3 then add 1 so number is 1 to 3
-Assign Rock to 1
-Assign Paper to 2
-Assign Scissors to 3
-
-function check takes user input and computer input
-If user input is rock
-    Check if computer chose paper
-        If so, then computer wins
-    Else if computer chose scissors
-        Computer lost
-    Else 
-        Tie
-If user input is Paper
-    Check if computer chose scissors
-        If so, then computer wins
-    Else if computer chose rock
-        computer lost
-    Else
-        tie
-If user input is scissors
-    Check if computer chose rock
-        If so, then computer wins
-    Else if computer chose paper
-        computer lost
-    Else
-        tie
-
-Can i streamline this?
-check if computer and user are same
-    if not, continue
-if user is rock
-    pass to new function what beats rock (scissors), and computerchoice
-        if computer equals beat, user loses
-        else computer loses
-Repeat scissors and paper
-
-*/
-
-
-
+//Gets computer choice
 function makeComputerChoice() {
-    let randNum = Math.floor(Math.Random * 3) + 1;
-    console.log("rn: " + randNum);
+    //Sets randNum to 1, 2, or 3
+    let randNum = Math.floor(Math.random() * 3) + 1;
+    //sets computer choice based on randNum value
     let choice = randNum == 1 ? "Rock" : randNum == 2 ? "Paper" : "Scissors";
+    //computer choice is printed to console if you're curious
     console.log("choice: " + choice);
     return choice;
 }
 
+//Gets User Choice with error handling if player is snarky
 function getUserInput() {
-    let userInput = prompt("Rock, Paper, or Scissors?").toUpperCase();
-    while (!userInput && !(userInput == "ROCK" || userInput == "PAPER"
-    || userInput == "SCISSORS")) {
+    let userInput = "";
+
+    //prompt for user choice. if user gives invalid response
+    //itll keep repeating until valid response is given
+    do {
         userInput = prompt("Rock, Paper, or Scissors?").toUpperCase();
     }
+    while (!userInput || !(userInput == "ROCK" || userInput == "PAPER"
+    || userInput == "SCISSORS"));
+
+    //converts user response to first letter capitalized string
+    //for later function reactability
     let userFirst = userInput.slice(0,1).toUpperCase();
     let userRest = userInput.slice(1).toLowerCase();
     userInput = userFirst + userRest;
+    //prints users finished input if curious
     console.log(userInput);
+    return userInput;
 }
 
+//Checks users choice and reacts accordingly
+function checkUserChoice(userInput, compChoice) {
+    //checking for tie first makes rest of checks simplified
+    if (userInput == compChoice) {
+        return "Tie!";
+    } else if (userInput == "Rock") {
+        //sends the choice that would make user lose
+        //compChoice is sent for comparative
+        //userInput only sent for result string
+        return checkWinner("Paper", userInput, compChoice);
+    } else if (userInput == "Paper") {
+        return checkWinner("Scissors", userInput, compChoice);
+    } else if (userInput == "Scissors") {
+        return checkWinner("Rock", userInput, compChoice)
+    } else {
+        //only hits if something went wrong
+        console.log("yuh oh");
+    }
+}
+
+//Determines the winner of the game
 function checkWinner(userLose, userInput, compChoice) {
+    //since tie is already checked, only have to check if compChoice
+    //is or isnt the choice to make user lose
     if (compChoice == userLose) {
         return `You Lose! ${userLose} beats ${userInput}`;
     } else {
@@ -75,16 +62,8 @@ function checkWinner(userLose, userInput, compChoice) {
     }
 }
 
-function checkUserChoice(userInput, compChoice) {
-    if (userInput == compChoice) {
-        return "Tie!";
-    } else if (userInput == "Rock") {
-        return check("Paper", userInput, compChoice);
-    } else if (userInput == "Paper") {
-        return check("Scissors", userInput, compChoice);
-    } else if (userInput == "Scissors") {
-        return check("Rock", userInput, compChoice)
-    } else {
-        console.log("yuh oh");
-    }
-}
+//runs the game once, then checks if user wants to keep playing
+//game repeats until user doesn't want to play anymore
+do {
+    alert(checkUserChoice(getUserInput(), makeComputerChoice()));
+} while (confirm("Play Again?"));
