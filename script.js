47ec8bbc10092088acfd94b79af7339e3a541b80
Rayden22 <29240299+Rayden5@users.noreply.github.com>
2023-10-31 11:59:37 -0700

    Increase comment and code readability
    Tried to reduce the amount of comments used to let code speak for
    itself. Left comments explaining the logic or why of some parts.
    
    [Ticket: 4]

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
-//Gets computer choice for the game
+
 function makeComputerChoice() {
     //randNum is 1, 2, or 3 for each game choice
     let randNum = Math.floor(Math.random() * 3) + 1;
     let choice = randNum == 1 ? "Rock" : randNum == 2 ? "Paper" : "Scissors";
-    //computer choice is printed to console if you're curious
-    console.log("choice: " + choice);
+    
+    console.log("computer choice: " + choice);
     return choice;
 }
 
-//Gets User Choice for the game
 function getUserInput() {
     let userInput = "";
 
-    //prompt for user choice. Loop for if player gives no response or
-    //invalid response
     do {
         userInput = prompt("Rock, Paper, or Scissors?").toUpperCase();
     }
+    //Catch missing or invalid user response
     while (!userInput || !(userInput == "ROCK" || userInput == "PAPER"
     || userInput == "SCISSORS"));
 
-    //converts user response for later function readibility
+    //Capitalizes only first letter for later function reactability
     let userFirst = userInput.slice(0,1).toUpperCase();
     let userRest = userInput.slice(1).toLowerCase();
     userInput = userFirst + userRest;
-    //prints users finished input if curious
-    console.log(userInput);
+    
+    console.log("UserInput: " + userInput);
     return userInput;
 }
 
-//Checks user choice to see what computer needs to lose
 function checkUserChoice(userInput, compChoice) {
-    //tie check to reduce repitition in each choice check
+    //first is tie check to reduce repitition in each choice check
     if (userInput == compChoice) {
         return "Tie!";
     } else if (userInput == "Rock") {
-        //first arguement for easy player loss checking
         return decideWinner("Paper", userInput, compChoice);
     } else if (userInput == "Paper") {
         return decideWinner("Scissors", userInput, compChoice);
-    } else if (userInput == "Scissors") {
+    } else (userInput == "Scissors") {
         return decideWinner("Rock", userInput, compChoice);
-    } else {
-        //only hits if something went wrong
-        console.log("yuh oh");
     }
 }
 
-//Decides winner for result string
-function decideWinner(userLose, userInput, compChoice) {
-    //since tie is already checked, only have to check if compChoice
-    //is or isnt the choice to make user lose
-    if (compChoice == userLose) {
-        return `You Lose! ${userLose} beats ${userInput}`;
+function decideWinner(userLoses, userInput, compChoice) {
+    //tie is already checked, so its between winning and losing
+    if (compChoice == userLoses) {
+        return `You Lose! ${userLoses} beats ${userInput}`;
     } else {
         return `Winner! ${userInput} beats ${compChoice}`;
     }
 }
 
-//Runs game. duh
 do {
     alert(checkUserChoice(getUserInput(), makeComputerChoice()));
 } while (confirm("Play Again?"));
